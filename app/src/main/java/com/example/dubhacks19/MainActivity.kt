3ee74188a986c83f97c2f456e84566ced71f4df0
saralyns <saralyns@uw.edu>
2019-10-13 01:01:25 -0700

    initial

---
+++ app/src/main/java/com/example/dubhacks19/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dubhacks19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.camera)
+    }
+}
